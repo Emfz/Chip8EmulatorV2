96e368c0c12b457e0754e99f239b5cf5233b7c16
Emfz <enrique.mfz@outlook.com>
2023-02-13 18:41:44 -0600

    Create the tests and corresponding code for the basic functionality of the Chip 8 screen.

---
+++ chip8_screen.py
@@ -0,0 +1,125 @@
+from pygame import SCALED, FULLSCREEN, PixelArray
+from pygame.display import init, set_mode, get_surface, get_init, flip
+from pygame.pixelcopy import array_to_surface, surface_to_array
+import numpy
+
+white = 0xFFFFFF
+
+class Chip8Screen:
+	def __init__(self) -> None:
+		self.initialize()
+
+	def initialize(self) -> None:
+		"""
+		Initializes the screen by setting its width, height, "scaled and fullscreen" flags, storing
+		the surface in aa variable and creaating a pixel array to modify each individual pixel.
+		"""
+		self.width = 64
+		self.height = 32
+		init()
+		set_mode(size=(self.width, self.height), flags = SCALED | FULLSCREEN)
+		self.surface = get_surface()
+		self.pixel_array = PixelArray(self.surface)
+	
+	def get_init(self) -> bool:
+		"""
+		Get the init state of the pygame display.
+
+		Returns
+		-------
+		True if the display is initialized, False otherwise.
+		"""
+		return get_init()
+	
+	def update(self) -> None:
+		"""
+		Display the pixels inside the surface in the display.
+		"""
+		flip()
+
+	def set_pixels(self, pixel_array:numpy.ndarray) -> None:
+		"""
+		Send a complete buffer to the screen. This method is for testing and development, because this funcionality
+		isn't part of the Chip 8 standard.
+
+		Parameters
+		------------
+		pixel_array : A numpy.ndarray of dimensions (64, 32) with the color of each pixel represented 
+		by an integer hexadecimal number 0xNNNNNN.
+		"""
+		array_to_surface(self.surface, pixel_array)
+
+	def clear(self) -> None:
+		"""
+		Sets all pixels of the screen to black.
+		"""
+		array_to_surface(self.surface, numpy.zeros(shape=(self.width, self.height), dtype=int))
+
+	def get_state(self) -> numpy.ndarray:
+		"""
+		Get the current state of the pixels of the screen.
+
+		Returns
+		-------
+		A numpy.ndarray of dimensions (64,32) containing the current value of every pixel.
+		"""
+		state_array = numpy.ndarray(shape=(self.width, self.height), dtype=int)
+		surface_to_array(state_array, self.surface)
+		return state_array
+	
+	def draw_sprite(self, sprite:numpy.ndarray, x:int, y:int) -> bool:
+		"""
+		Draw a sprite 8 pixels wide and specifiable height at coordinates (x,y). The pixels of the screen are xor'd with the sprite's pixels,
+		so if the current sprite bit = 1, the screen pixel is toggled. If the sprite's bit = 0, the screen pixel is left unchanged.
+
+		Parameters
+		----------
+		sprite : A numpy.ndarray 1-D array that meets the following conditions.
+		1) Each element of the array is a hexadecimal integer number 0xNN, representing 8 pixels (a byte) in a row.
+		2) The value of each of the 8 bits of the number 0xNN dictates whether the corresponding pixel screen will be toggled 
+		(sprite bit = 1) or left alone (sprite bit = 0).
+		3) The height of the sprite is specified by the number of elements of the array.
+		
+		x : Horizontal coordinate where the top left corner of the sprite will be located.
+		y : Vertical coordinate where the top left corner of the sprite will be located.
+
+		Returns
+		-------
+		True if any of the screen pixels were toggled from 1 to 0, False otherwise
+
+		Example
+		-------
+		draw_sprite(numpy.array([0xFF, 0xAA]), 2, 10) will draw a sprite 8 pixels wide and 2 pixels tall at coordinates (2,10).
+		The sprite will have the following pattern:\\
+		■ ■ ■ ■ ■ ■ ■ ■ \\
+		■ □ ■ □ ■ □ ■ □
+
+		"""
+		# Logic (not bitwise) xor function to toggle screen pixels
+		xor = lambda a,b : (a and not b) or (b and not a)
+
+		state = self.get_state()
+		result_array = numpy.zeros(shape=(self.width, self.height), dtype=int)
+		screen_pixel_flipped = False
+		for byte in sprite:
+			mask = 0b10000000
+			iteration = 0
+			while mask > 0:
+				try:
+					# Get a single bit from the sprite's byte
+					bit = byte & mask
+					# Toggle the current screen pixel if the sprite's bit = 1, set to 0 otherwise.
+					result_array[x + iteration, y] = xor(bit, state[x + iteration, y]) and white
+					# Check if the current screen pixel was toggled from 1 to 0
+					if bit and state[x + iteration, y]:
+						screen_pixel_flipped = True
+				except IndexError:
+					# If the current pixel is outside of the screen, do nothing
+					pass
+				finally:
+					mask >>= 1
+					iteration += 1
+			y += 1
+
+		array_to_surface(self.surface, result_array)
+		return screen_pixel_flipped
---
+++ test_screen.py
@@ -0,0 +1,163 @@
+import pytest
+import numpy
+from chip8_screen import Chip8Screen
+
+screen = Chip8Screen()
+white = 0xFFFFFF
+
+@pytest.fixture
+def full_white_buffer():
+	return numpy.full((screen.width, screen.height), white)
+
+@pytest.fixture
+def full_black_buffer():
+	return numpy.full((screen.width, screen.height), 0)
+
+@pytest.fixture
+def invalid_array():
+	return numpy.full((41,85), white)
+
+@pytest.fixture
+def sprite1():
+	return numpy.array([0x00, 0x00, 0x00, 0x36, 0x7F, 0x6B, 0x55, 0x00])
+
+@pytest.fixture
+def sprite2():
+	return numpy.array([0xB4, 0x4C])
+
+@pytest.fixture
+def sprite3():
+	return numpy.array([0xAA])
+
+
+
+def test_screen_init():
+	assert screen.get_init() == True
+
+def test_set_clear_screen(full_white_buffer, full_black_buffer):
+	screen.set_pixels(full_white_buffer)
+	screen.update()
+	assert numpy.array_equal(screen.get_state(), full_white_buffer)
+
+	screen.clear()
+	screen.update()
+	assert numpy.array_equal(screen.get_state(), full_black_buffer)
+
+def test_invalid_array_dimensions(invalid_array):
+	with pytest.raises(ValueError):	
+		screen.clear()
+		screen.set_pixels(invalid_array)
+		screen.update()
+
+def test_draw_sprite(sprite1):
+	x = 4
+	y = 0
+	screen.clear()
+	pixels_flipped = screen.draw_sprite(sprite1, x, y)
+	screen.update()
+	state = screen.get_state()
+
+	manual_array = numpy.zeros(shape=(screen.width, screen.height), dtype=int)
+	
+	# Manually modify the array data with the spray data 
+	manual_array[4,3] = 0
+	manual_array[5,3] = 0
+	manual_array[6,3] = white
+	manual_array[7,3] = white
+	manual_array[8,3] = 0
+	manual_array[9,3] = white
+	manual_array[10,3] = white
+	manual_array[11,3] = 0
+
+	manual_array[4,4] = 0
+	manual_array[5,4] = white
+	manual_array[6,4] = white
+	manual_array[7,4] = white
+	manual_array[8,4] = white
+	manual_array[9,4] = white
+	manual_array[10,4] = white
+	manual_array[11,4] = white
+
+	manual_array[4,5] = 0
+	manual_array[5,5] = white
+	manual_array[6,5] = white
+	manual_array[7,5] = 0
+	manual_array[8,5] = white
+	manual_array[9,5] = 0
+	manual_array[10,5] = white
+	manual_array[11,5] = white
+
+	manual_array[4,6] = 0
+	manual_array[5,6] = white
+	manual_array[6,6] = 0
+	manual_array[7,6] = white
+	manual_array[8,6] = 0
+	manual_array[9,6] = white
+	manual_array[10,6] = 0
+	manual_array[11,6] = white
+	
+	assert numpy.array_equal(state, manual_array)
+	assert pixels_flipped == False
+
+def test_draw_sprite_out_of_bounds_1(sprite2):
+	x = 62
+	y = 30
+
+	screen.clear()
+	pixels_flipped = screen.draw_sprite(sprite2, x, y)
+	screen.update()
+	state = screen.get_state()
+
+	manual_array = numpy.zeros(shape=(screen.width, screen.height), dtype=int)
+	manual_array[62,30] = white
+	manual_array[63,30] = 0
+
+	manual_array[62,31] = 0
+	manual_array[63,31] = white
+
+	assert numpy.array_equal(state, manual_array)
+	assert pixels_flipped == False
+
+def test_draw_sprite_out_of_bounds_2(sprite2):
+	x = 70
+	y = 40
+
+	screen.clear()
+	pixels_flipped = screen.draw_sprite(sprite2, x, y)
+	screen.update()
+	state = screen.get_state()
+
+	manual_array = numpy.zeros(shape=(screen.width, screen.height), dtype=int)
+
+	assert numpy.array_equal(state, manual_array)
+	assert pixels_flipped == False
+
+def test_check_if_drawsprite_toggles_pixels(sprite3):
+	x = 0
+	y = 0
+
+	screen.clear()
+	pixels_flipped = screen.draw_sprite(sprite3, x, y)
+	screen.update()
+	state = screen.get_state()
+
+	manual_array = numpy.zeros(shape=(screen.width, screen.height), dtype=int)
+	manual_array[0,0] = white
+	manual_array[1,0] = 0
+	manual_array[2,0] = white
+	manual_array[3,0] = 0
+	manual_array[4,0] = white
+	manual_array[5,0] = 0
+	manual_array[6,0] = white
+	manual_array[7,0] = 0
+
+	assert numpy.array_equal(state, manual_array)
+	assert pixels_flipped == False
+
+	pixels_flipped = screen.draw_sprite(sprite3, x, y)
+	screen.update()
+	state = screen.get_state()
+
+	manual_array = numpy.zeros(shape=(screen.width, screen.height), dtype=int)
+	assert numpy.array_equal(state, manual_array)
+	assert pixels_flipped == True
