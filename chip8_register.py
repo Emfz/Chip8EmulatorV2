3254f54dad00867f1d489604f229ed3fdc6b7ca5
Emfz <enrique.mfz@outlook.com>
2023-02-15 15:50:14 -0600

    Refactor the register code to define the memory register. Both register types (regular and memory) now inherit from a common register interface.

--- chip8_register.py
+++ chip8_register.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 
-class Chip8Register:
+class Chip8RegisterInterface(ABC):
 	def __init__(self) -> None:
 		self.state = 0
 
@@ -10,10 +11,15 @@ class Chip8Register:
 
 		Returns
 		-------
-		The integer currently stored.
+		The integer that's currently stored.
 		"""
 		return self.state
-	
+
+	@abstractmethod
+	def set(self, value):
+		pass
+
+class Chip8Register(Chip8RegisterInterface):
 	def set(self, value:int|Chip8Register) -> None:
 		"""
 		Store a value in this register. Chip 8 registers can only store 1 byte of data, therefore, storing a number larger than
@@ -26,3 +32,15 @@ class Chip8Register:
 		if type(value) is Chip8Register:
 			value = value.get_state()
 		self.state = value & 0xFF
+
+class Chip8MemoryRegister(Chip8RegisterInterface):
+	def set(self, value:int) -> int:
+		"""
+		Store a value in this register. The Chip 8 memory register is 16 bits long, but only 12 bits are needed for the 4k memory. 
+		Therefore, attempting to store a number larger than 12 bits (0xFFF) will result in data loss.
+
+		Parameters
+		----------
+		value : An integer ranging from 0x0 to 0xFFF
+		"""
+		self.state = value & 0xFFF
--- test_registers.py
+++ test_registers.py
@@ -1,9 +1,11 @@
 import pytest
 
-from chip8_register import Chip8Register
+from chip8_register import Chip8Register, Chip8MemoryRegister
 
 register1 = Chip8Register()
 register2 = Chip8Register()
+
+memory_register = Chip8MemoryRegister()
 
 def test_set_register():
 	register1.set(0xA8)
@@ -20,3 +22,13 @@ def test_overflow_underflow_data_loss():
 	register1.set(-0xFFFF)
 	assert register1.get_state() == 0x1
 
+def test_set_memory_register():
+	memory_register.set(0xF4C)
+	assert memory_register.get_state() == 0xF4C
+
+def test_memory_register_overflow_underflow():
+	memory_register.set(0x23AB)
+	assert memory_register.get_state() == 0x3AB
+
+	memory_register.set(-0x1)
+	assert memory_register.get_state() == 0xFFF
